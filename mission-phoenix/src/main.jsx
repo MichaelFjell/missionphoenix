585cf931a2403ab4b07d6d8b5e6d5ab5f2bc0d7a
Michael Fjellström <fjellmicha@proton.me>
2026-03-30 10:25:31 +0200

    Delete mission-phoenix directory

--- mission-phoenix/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MissionPhoenix from './MissionPhoenix.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MissionPhoenix />
-  </React.StrictMode>,
-)
